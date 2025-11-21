e6f8c03c728bce0c7a7dd49b90affaf4d53e4081
Liyema Mkebe <liyema.mkebe15@gmail.com>
2025-11-21 23:53:21 +0200

    Added buttons to cards in services section
    - Testing new intersection observer for service cards

--- js/animations.js
+++ js/animations.js
@@ -21,21 +21,53 @@ navLinks.addEventListener("click", (e) => {
     // Changes menu-btn icon in close state
     menuBtnIcon.setAttribute("class", "ri-menu-line"); 
 })
+
+
  
-// Text On-Page load animation 
+// On-Page load animations 
 document.addEventListener("DOMContentLoaded", function() {
     const containers = document.querySelectorAll('.text-anim');
+    const serviceContainers = document.querySelectorAll('.service-anim');
+
+    // Observer for any text(or other) animation:
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('visible'); // if intersecting add .visible class
-                observer.unobserve(entry.target); // Not intersecting then stops observing .visible class or use:
-            }/*else{
+                // observer.unobserve(entry.target); // Not intersecting then stops observing .visible class or use:
+            }else{
                 // Not intersecting then remove .visible class
                 entry.target.classList.remove('visible');
-            }*/
+            }
         });
     }, { threshold: 0.3 });
 
+    // Observer for service cards(responsive):
+    const desktopCardsObserve = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+            entries[0].target.classList.add("visible");
+        }else{
+            entries[0].target.classList.remove("visible");
+        }
+    }, 
+    {
+        threshold: 0.3 
+    });
+
+    const mobileCardsObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.classList.add("visible");
+            }else{
+                entry.classList.remove("visible");
+            }
+        });
+    }, 
+    {
+        rootMargin: "0px 0px -400px 400px"
+    });
+    
+    window.innerWidth < 600 ? (serviceContainers.forEach(serviceContainer => mobileCardsObserver.observe(serviceContainer))) : (desktopCardsObserve.observe(serviceContainers));
+
     containers.forEach(container => observer.observe(container)); // Observe each container seperately
-}); 
+});

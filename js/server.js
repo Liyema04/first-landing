23f06c2079ca5fa918b11121935fe0c5187e1892
Liyema Mkebe <liyema.mkebe15@gmail.com>
2025-12-25 23:46:15 +0200

    Configuration for local HTTP server (HTML - loaded)

--- js/server.js
+++ js/server.js
@@ -1,13 +1,29 @@
 // Configuration for local HTTP server
 // Basic server first: 
 const http = require('http');
+const fs = require('fs').promises;
+const path = require('path');
+
 const hostname = 'localhost';
 const port = 3000;
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hello, Centurion! Welcome to my Node.js server for Apex (Demo-website).\n');
+
+const server = http.createServer(async(req, res) => {
+    try {
+        if (req.url === '/') {
+            const content = await fs.readFile(path.join(__dirname, '..', 'index.html'), 'utf8');
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+            res.end(content);
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.end('<h1>404 Not Found </h1>');
+        }
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Internal Server Error');
+    }
 });
 
+// Console status for dev tools: 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
